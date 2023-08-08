04e074c7002205a4e08bbc15c08ddd08170684c7
Chantapat Tancharoen <chantapat.t@kbtg.tech>
2023-08-08 17:17:44 +0700

    api for MiniRedisClient

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -1,12 +1,12 @@
 package client_test
 
 import (
-	"crypto/tls"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"log"
 	"mini-redis-go/pkg/client"
 	"mini-redis-go/pkg/config"
+	"mini-redis-go/pkg/core/redis"
 	"mini-redis-go/pkg/server"
 	"os"
 	"path/filepath"
@@ -27,37 +27,50 @@ func startServer(host, port, cacheFolder string) server.MiniRedisServer {
 	return s
 }
 
-func connectToServer(host, port string) *tls.Conn {
+func connectToServer(host, port string) client.MiniRedisClient {
 	c := client.NewClient(host, port, config.ClientPublicKeyFile, config.ClientPrivateKeyFile)
-	conn := c.Connect()
-	return conn
+	c.Connect()
+	return c
 }
 
-func write(t *testing.T, conn *tls.Conn, s string) {
-	if _, err1 := (*conn).Write([]byte(s)); err1 != nil {
-		t.Error("Error sending a message", err1)
+func set(t *testing.T, c client.MiniRedisClient, k string, v string) {
+	if err := c.Set(k, v); err != nil {
+		t.Error("Error set", err)
 	}
 }
 
-func read(t *testing.T, conn *tls.Conn) string {
-	buf := make([]byte, 1024)
-	n, err := (*conn).Read(buf)
+func get(t *testing.T, c client.MiniRedisClient, k string) string {
+	response, err := c.Get(k)
 	if err != nil {
-		t.Error("Error reading response", err)
+		t.Error("Error get", err)
 	}
-	return string(buf[:n])
+	return response
 }
+
+func write(t *testing.T, c client.MiniRedisClient, message []byte) {
+	if err := c.Write(message); err != nil {
+		t.Error("Error sending a message", err)
+	}
+}
+
+func read(t *testing.T, c client.MiniRedisClient) string {
+	response, err := c.Read()
+	if err != nil {
+		t.Error("Error reading from server", err)
+	}
+	return response
+}
+
 func TestPingPong(t *testing.T) {
 	tempFolder := createTempFolder()
 	defer func(path string) {
 		_ = os.RemoveAll(path)
 	}(tempFolder)
 	s := startServer(config.ConnectionHost, config.ConnectionPort, tempFolder)
-	conn := connectToServer(config.ConnectionHost, config.ConnectionPort)
+	c := connectToServer(config.ConnectionHost, config.ConnectionPort)
 
-	write(t, conn, "PING\n")
-	response := read(t, conn)
-
+	write(t, c, []byte("PING\n"))
+	response := read(t, c)
 	assert.Equal(t, "PONG\n", response)
 
 	s.Stop()
@@ -69,14 +82,13 @@ func TestSetAndGet(t *testing.T) {
 		_ = os.RemoveAll(path)
 	}(tempFolder)
 	s := startServer(config.ConnectionHost, config.ConnectionPort, tempFolder)
-	conn := connectToServer(config.ConnectionHost, config.ConnectionPort)
+	c := connectToServer(config.ConnectionHost, config.ConnectionPort)
 
-	write(t, conn, "set hello world\n")
-	response1 := read(t, conn)
+	set(t, c, "hello", "world")
+	response1 := read(t, c)
 	assert.Equal(t, "Set ok\n", response1)
 
-	write(t, conn, "get hello\n")
-	response2 := read(t, conn)
+	response2 := get(t, c, "hello")
 	assert.Equal(t, "world\n", response2)
 
 	s.Stop()
@@ -88,16 +100,35 @@ func TestCache(t *testing.T) {
 		_ = os.RemoveAll(path)
 	}(tempFolder)
 	fmt.Println("tempFolder", tempFolder)
-	createCacheFileWithData(tempFolder, "testKey", []byte("tesValue"))
+	createCacheFileWithData(tempFolder, "testKey", append([]byte{byte(redis.StringByteType)}, []byte("tesValue")...))
 	s := startServer(config.ConnectionHost, config.ConnectionPort, tempFolder)
-	conn := connectToServer(config.ConnectionHost, config.ConnectionPort)
+	c := connectToServer(config.ConnectionHost, config.ConnectionPort)
 
-	write(t, conn, "get testKey\n")
-	response2 := read(t, conn)
-	assert.Equal(t, "tesValue\n", response2)
+	response := get(t, c, "testKey")
+	assert.Equal(t, "tesValue\n", response)
 
 	s.Stop()
 }
+
+//func TestSubscriberAndPublish(t *testing.T) {
+//	tempFolder := createTempFolder()
+//	defer func(path string) {
+//		_ = os.RemoveAll(path)
+//	}(tempFolder)
+//	s := startServer(config.ConnectionHost, config.ConnectionPort, tempFolder)
+//	client1 := connectToServer(config.ConnectionHost, config.ConnectionPort)
+//	client3 := connectToServer(config.ConnectionHost, config.ConnectionPort)
+//
+//	//write(t, conn, "set hello world\n")
+//	//response1 := read(t, conn)
+//	//assert.Equal(t, "Set ok\n", response1)
+//	//
+//	//write(t, conn, "get hello\n")
+//	//response2 := read(t, conn)
+//	//assert.Equal(t, "world\n", response2)
+//
+//	s.Stop()
+//}
 
 func createCacheFileWithData(folder string, k string, v []byte) {
 	file, _ := os.OpenFile(filepath.Join(folder, k), os.O_CREATE|os.O_WRONLY, 0644)
